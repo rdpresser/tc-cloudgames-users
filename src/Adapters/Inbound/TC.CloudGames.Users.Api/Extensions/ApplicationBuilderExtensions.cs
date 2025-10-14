4e63e2f28b86a8920a9b205afd9b6f1581932219
rdpresser <rodrigo.presser@gmail.com>
2025-10-14 13:25:07 -0300

    Add proxy headers support in UseCustomMiddlewares method

--- src/Adapters/Inbound/TC.CloudGames.Users.Api/Extensions/ApplicationBuilderExtensions.cs
+++ src/Adapters/Inbound/TC.CloudGames.Users.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -1,4 +1,5 @@
-﻿using TC.CloudGames.SharedKernel.Infrastructure.Database.Initializer;
+﻿using Microsoft.AspNetCore.HttpOverrides;
+using TC.CloudGames.SharedKernel.Infrastructure.Database.Initializer;
 
 namespace TC.CloudGames.Users.Api.Extensions
 {
@@ -59,6 +60,9 @@ namespace TC.CloudGames.Users.Api.Extensions
         // Configures custom middlewares including HTTPS redirection, exception handling, correlation, logging, and health checks
         public static IApplicationBuilder UseCustomMiddlewares(this IApplicationBuilder app)
         {
+            // Enables proxy headers (important for ACA)
+            app.UseForwardedHeaders(new ForwardedHeadersOptions { ForwardedHeaders = ForwardedHeaders.All });
+
             app.UseCustomExceptionHandler()
                 .UseCorrelationMiddleware()
                 .UseMiddleware<TelemetryMiddleware>() // Add telemetry middleware after correlation
